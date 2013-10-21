47e5af30b1736d8fdf1725da3073dd279780bfe8
al_skipp <al_skipp@fastmail.fm>
2013-10-21 16:59:58 +0100

    Added a few example sliders with custom values

--- ValueTrackingSlider/ASViewController.m
+++ ValueTrackingSlider/ASViewController.m
@@ -10,7 +10,9 @@
 #import "ASValueTrackingSlider.h"
 
 @interface ASViewController ()
-@property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider;
+@property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider1;
+@property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider2;
+@property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider3;
 @end
 
 @implementation ASViewController
@@ -18,11 +20,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.slider.minimumValue = 0.0;
-    self.slider.maximumValue = 255.0;
-    [self.slider setMaxFractionDigitsDisplayed:2];
-    [self.slider setPopUpViewColor:[UIColor colorWithHue:0.55 saturation:0.5 brightness:0.9 alpha:0.8]];
-    [self.slider setTextColor:[UIColor colorWithHue:0.55 saturation:1 brightness:0.4 alpha:1]];
+    
+    // customize slider 1
+    self.slider1.maximumValue = 2.0;
+
+    
+    // customize slider 2
+    self.slider2.maximumValue = 255.0;
+    [self.slider2 setMaxFractionDigitsDisplayed:0];
+    [self.slider2 setPopUpViewColor:[UIColor colorWithHue:0.55 saturation:0.5 brightness:0.9 alpha:0.8]];
+    [self.slider2 setTextColor:[UIColor colorWithHue:0.55 saturation:1 brightness:0.4 alpha:1]];
+    
+    
+    // customize slider 3
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    [formatter setNumberStyle:NSNumberFormatterPercentStyle];
+    [self.slider3 setNumberFormatter:formatter];
+    [self.slider3 setPopUpViewColor:[UIColor colorWithHue:0.4 saturation:0.9 brightness:0.7 alpha:1]];
 }
 
 - (void)didReceiveMemoryWarning
