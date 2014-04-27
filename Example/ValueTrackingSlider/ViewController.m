9261b62fec53f05d020ef125a7d0b651fa2808d9
al_skipp <al_skipp@fastmail.fm>
2014-04-28 00:04:28 +0100

    Added switch to control slider movement animation
    Removed one slider from example to make space for UISwitch.

--- Example/ValueTrackingSlider/ViewController.m
+++ Example/ValueTrackingSlider/ViewController.m
@@ -13,7 +13,8 @@
 @property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider1;
 @property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider2;
 @property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider3;
-@property (weak, nonatomic) IBOutlet ASValueTrackingSlider *slider4;
+
+@property (weak, nonatomic) IBOutlet UISwitch *animateSwitch;
 @end
 
 @implementation ViewController
@@ -26,36 +27,32 @@
     [super viewDidLoad];
     
     // customize slider 1
-    self.slider1.maximumValue = 2.0;
+    self.slider1.maximumValue = 255.0;
+    [self.slider1 setMaxFractionDigitsDisplayed:0];
+    self.slider1.popUpViewColor = [UIColor colorWithHue:0.55 saturation:0.8 brightness:0.9 alpha:0.7];
+    self.slider1.font = [UIFont fontWithName:@"GillSans-Bold" size:22];
+    self.slider1.textColor = [UIColor colorWithHue:0.55 saturation:1.0 brightness:0.5 alpha:1];
 
     
     // customize slider 2
-    self.slider2.maximumValue = 255.0;
-    [self.slider2 setMaxFractionDigitsDisplayed:0];
-    self.slider2.popUpViewColor = [UIColor colorWithHue:0.55 saturation:0.8 brightness:0.9 alpha:0.7];
-    self.slider2.font = [UIFont fontWithName:@"GillSans-Bold" size:22];
-    self.slider2.textColor = [UIColor colorWithHue:0.55 saturation:1.0 brightness:0.5 alpha:1];
-
-    
-    // customize slider 3
     NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
     [formatter setNumberStyle:NSNumberFormatterPercentStyle];
-    [self.slider3 setNumberFormatter:formatter];
-    self.slider3.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:26];
-    self.slider3.popUpViewAnimatedColors = @[[UIColor purpleColor], [UIColor redColor], [UIColor orangeColor]];
+    [self.slider2 setNumberFormatter:formatter];
+    self.slider2.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:26];
+    self.slider2.popUpViewAnimatedColors = @[[UIColor purpleColor], [UIColor redColor], [UIColor orangeColor]];
     
     
-    // customize slider 4
+    // customize slider 3
     NSNumberFormatter *tempFormatter = [[NSNumberFormatter alloc] init];
     [tempFormatter setPositiveSuffix:@"°C"];
     [tempFormatter setNegativeSuffix:@"°C"];
     
-    [self.slider4 setNumberFormatter:tempFormatter];
-    self.slider4.minimumValue = -20.0;
-    self.slider4.maximumValue = 60.0;
+    [self.slider3 setNumberFormatter:tempFormatter];
+    self.slider3.minimumValue = -20.0;
+    self.slider3.maximumValue = 60.0;
     
-    self.slider4.font = [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:26];
-    self.slider4.textColor = [UIColor colorWithWhite:0.0 alpha:0.5];
+    self.slider3.font = [UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:26];
+    self.slider3.textColor = [UIColor colorWithWhite:0.0 alpha:0.5];
     
     UIColor *coldBlue = [UIColor colorWithHue:0.6 saturation:0.7 brightness:1.0 alpha:1.0];
     UIColor *blue = [UIColor colorWithHue:0.55 saturation:0.75 brightness:1.0 alpha:1.0];
@@ -63,10 +60,10 @@
     UIColor *yellow = [UIColor colorWithHue:0.15 saturation:0.9 brightness:0.9 alpha:1.0];
     UIColor *red = [UIColor colorWithHue:0.0 saturation:0.8 brightness:1.0 alpha:1.0];
 
-    [self.slider4 setPopUpViewAnimatedColors:@[coldBlue, blue, green, yellow, red]
+    [self.slider3 setPopUpViewAnimatedColors:@[coldBlue, blue, green, yellow, red]
                                withPositions:@[@-20, @0, @5, @25, @60]];
     
-    _sliders = @[_slider1, _slider2, _slider3, _slider4];
+    _sliders = @[_slider1, _slider2, _slider3];
 }
 
 - (IBAction)toggleShowHide:(UIButton *)sender
@@ -80,23 +77,38 @@
 - (IBAction)moveSlidersToMinimum:(UIButton *)sender
 {
     for (ASValueTrackingSlider *slider in _sliders) {
-        slider.value = slider.minimumValue;
+        if (self.animateSwitch.on) {
+            [self animateSlider:slider toValue:slider.minimumValue];
+        }
+        else {
+            slider.value = slider.minimumValue;
+        }
+    }
+}
+
+- (IBAction)moveSlidersToMaximum:(UIButton *)sender
+{
+    for (ASValueTrackingSlider *slider in _sliders) {
+        if (self.animateSwitch.on) {
+            [self animateSlider:slider toValue:slider.maximumValue];
+        }
+        else {
+            slider.value = slider.maximumValue;
+        }
     }
 }
 
 // the behaviour of setValue:animated: is different between iOS6 and iOS7
 // need to use animation block on iOS7
-- (IBAction)moveSlidersToMaximum:(UIButton *)sender
+- (void)animateSlider:(ASValueTrackingSlider*)slider toValue:(float)value
 {
-    for (ASValueTrackingSlider *slider in _sliders) {
-        if (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_6_1) {
-            [UIView animateWithDuration:1.0 animations:^{
-                [slider setValue:slider.maximumValue animated:YES];
-            }];
-        }
-        else {
-            [slider setValue:slider.maximumValue animated:YES];
-        }
+    if (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_6_1) {
+        [UIView animateWithDuration:0.5 animations:^{
+            [slider setValue:value animated:YES];
+        }];
+    }
+    else {
+        [slider setValue:value animated:YES];
     }
 }
 
